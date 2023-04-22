c23fade332c92859eba057bc2cbc75dfce4abb66
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-04-22 09:16:39 -0300

    feat:app tabulation

--- src/main/java/lexico/com/esdras/org/example/App.java
+++ src/main/java/lexico/com/esdras/org/example/App.java
@@ -1,11 +1,51 @@
 package lexico.com.esdras.org.example;
 
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+
+        String nomeArquivo = "testedolexico.pas";
+
+        substituirTabulacao(nomeArquivo);
+
+        Token token = null;
+
+        do {
+
+        } while (token.getClasse() != Classe.cEOF);
+
+
+
         System.out.println( "Hello World!" );
+    }
+
+
+    public static void substituirTabulacao(String nomeArquivo) {
+        Path caminhoArquivo = Paths.get(nomeArquivo);
+        int numeroEspacosPorTab = 4;
+        StringBuilder juntando = new StringBuilder();
+        String espacos;
+
+        for (int cont = 0; cont < numeroEspacosPorTab; cont++) {
+            juntando.append(" ");
+        }
+        espacos = juntando.toString();
+
+        String conteudo;
+        try {
+            conteudo = new String(Files.readAllBytes(caminhoArquivo), StandardCharsets.UTF_8);
+            conteudo = conteudo.replace("\t", espacos);
+            Files.write(caminhoArquivo, conteudo.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ testedolexico.pas
@@ -0,0 +1,5 @@
+Esdras <> if
+teste3:=20>50+6array
+Teste4 >= 10.2
+Teste >= 87.1
+
