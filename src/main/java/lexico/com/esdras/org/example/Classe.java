6bb29754fcaac8d81e25d8a9fed115eabcc9050c
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-04-22 11:48:06 -0300

    feat:adding class token valor

---
+++ src/main/java/lexico/com/esdras/org/example/Classe.java
@@ -0,0 +1,28 @@
+package lexico.com.esdras.org.example;
+
+public enum Classe {
+    cId,
+    cInt,
+    cReal,
+    cPalRes,
+    cDoisPontos,
+    cAtribuicao,
+    cMais,
+    cMenos,
+    cDivisao,
+    cMultiplicacao,
+    cMaior,
+    cMenor,
+    cMaiorIgual,
+    cMenorIgual,
+    cDiferente,
+    cIgual,
+    cVirgula,
+    cPontoVirgula,
+    cPonto,
+    cParEsq,
+    cParDir,
+    cString,
+    cEOF,
+
+}
---
+++ src/main/java/lexico/com/esdras/org/example/Token.java
@@ -0,0 +1,10 @@
+package lexico.com.esdras.org.example;
+
+public class Token {
+    private Classe classe;
+    private Valor valor;
+    private int linha;
+    private int coluna;
+
+
+}
